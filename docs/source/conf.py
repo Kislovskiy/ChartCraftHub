b5b001a73e7da29d9d3b35518535a40bd2eadbc0
kislovskiy <kislovskiy@protonmail.com>
2024-03-23 00:18:22 +0100

    Revert "patch version"
    This reverts commit 1e8baef982be1e936c3d5aca1f017efb1c366d26.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "ChartCraftHub"
 copyright = "2024, Artem Kislovskiy, Teresa Kubacka"
 author = "Artem Kislovskiy, Teresa Kubacka"
-release = "1.0.1"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
